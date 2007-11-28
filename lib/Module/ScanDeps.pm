b09194368b3f1e00b47272d35acdfc1651cd97ca
tsee <tsee@5becfe18-24f1-4213-9d82-ccd00e63ea39>
2007-11-28 09:02:10 +0000

    Version bump to 0.80.

--- lib/Module/ScanDeps.pm
+++ lib/Module/ScanDeps.pm
@@ -4,7 +4,7 @@ use 5.004;
 use strict;
 use vars qw( $VERSION @EXPORT @EXPORT_OK $CurrentPackage @IncludeLibs $ScanFileRE );
 
-$VERSION   = '0.79';
+$VERSION   = '0.80';
 @EXPORT    = qw( scan_deps scan_deps_runtime );
 @EXPORT_OK = qw( scan_line scan_chunk add_deps scan_deps_runtime path_to_inc_name );
 
