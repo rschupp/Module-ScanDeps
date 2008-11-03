52d5b58733d0ecf20effd172399616f20fda8943
tsee <tsee@5becfe18-24f1-4213-9d82-ccd00e63ea39>
2008-11-03 21:35:21 +0000

    document changes for 0.89

--- lib/Module/ScanDeps.pm
+++ lib/Module/ScanDeps.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use vars qw( $VERSION @EXPORT @EXPORT_OK @ISA $CurrentPackage @IncludeLibs $ScanFileRE );
 
-$VERSION   = '0.88';
+$VERSION   = '0.89';
 @EXPORT    = qw( scan_deps scan_deps_runtime );
 @EXPORT_OK = qw( scan_line scan_chunk add_deps scan_deps_runtime path_to_inc_name );
 
