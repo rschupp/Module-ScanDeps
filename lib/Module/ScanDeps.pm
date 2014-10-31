78f3eea7894f6c1c5be93a841148bc73ab263dbd
rschupp <rschupp@5becfe18-24f1-4213-9d82-ccd00e63ea39>
2014-10-31 09:56:56 +0000

    add a %Preload rule: Mozilla::CA requires its cacert.pem file

--- lib/Module/ScanDeps.pm
+++ lib/Module/ScanDeps.pm
@@ -403,6 +403,7 @@ my %Preload;
         _glob_in_inc('MooseX/POE', 1),
         _glob_in_inc('MooseX/Async', 1),
     },
+    'Mozilla/CA.pm'                 => [qw( Mozilla/CA/cacert.pem )],
     'MozRepl.pm'                    => sub {
         qw( MozRepl/Log.pm MozRepl/Client.pm Module/Pluggable/Fast.pm ),
         _glob_in_inc('MozRepl/Plugin', 1),
